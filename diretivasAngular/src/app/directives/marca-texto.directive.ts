cb30d7d290cf558222c4bbb5eed7d70276cddae9
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-02-16 20:13:19 -0300

    criando uma diretiva personalizada

---
+++ diretivasAngular/src/app/directives/marca-texto.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appMarcaTexto]'
+})
+export class MarcaTextoDirective implements OnInit {
+  @Input() corDeFundo:string = 'yellow';
+  @Input() corDoTexto:string = 'blue';
+
+  constructor(private elemento: ElementRef) { }
+
+  ngOnInit(): void {
+    this.mudarFundo();
+  }
+
+  mudarFundo(cor:string = 'yellow'){
+    this.elemento.nativeElement.style.backgroundColor = this.corDeFundo || cor;
+    this.elemento.nativeElement.style.color = this.corDoTexto || cor;
+    this.elemento.nativeElement.style.fontWeight = 'bold';
+  }
+
+}
